c8720a05f705c359bf071c519019c12d25dd6c84
Patrick Policarpio <patrickpolicarpio08@gmail.com>
2024-05-17 14:15:46 +0800

    update camera screen

--- proj/screens/Camera.js
+++ proj/screens/Camera.js
@@ -1,30 +1,27 @@
-import React, { useState, useEffect } from 'react';
-import { View, StyleSheet, Text } from 'react-native';
-import { Camera } from 'expo-camera';
+import React, { useState, useEffect } from "react";
+import { View, StyleSheet, Text } from "react-native";
+import { Camera } from "expo-camera";
 
 const ImageClassifierScreen = () => {
-  const [cameraPermission, setCameraPermission] = useState(null);
+  // const [cameraPermission, setCameraPermission] = useState(null);
 
-  useEffect(() => {
-    (async () => {
-      const { status } = await Camera.requestPermissionsAsync();
-      setCameraPermission(status === 'granted');
-    })();
-  }, []);
+  // useEffect(() => {
+  //   (async () => {
+  //     const { status } = await Camera.requestPermissionsAsync();
+  //     setCameraPermission(status === 'granted');
+  //   })();
+  // }, []);
 
-  if (cameraPermission === null) {
-    return <View style={styles.container}><Text>Requesting camera permission...</Text></View>;
-  }
-  if (cameraPermission === false) {
-    return <View style={styles.container}><Text>No access to camera</Text></View>;
-  }
+  // if (cameraPermission === null) {
+  //   return <View style={styles.container}><Text>Requesting camera permission...</Text></View>;
+  // }
+  // if (cameraPermission === false) {
+  //   return <View style={styles.container}><Text>No access to camera</Text></View>;
+  // }
 
   return (
     <View style={styles.container}>
-      <Camera
-        style={styles.camera}
-        type={Camera.Constants.Type.back}
-      />
+      <Text>Camera</Text>
     </View>
   );
 };
@@ -32,7 +29,9 @@ const ImageClassifierScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'black',
+    backgroundColor: "white",
+    justifyContent: "center",
+    alignItems: "center",
   },
   camera: {
     flex: 1,
